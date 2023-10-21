093790bd285c16d46e2d115fcf9bc8b334793116
isaev nikita <isaev@e-krit.ru>
2023-10-21 14:44:27 +0300

    fix cookie

--- components/CookieAlert/CookieAlert.tsx
+++ components/CookieAlert/CookieAlert.tsx
@@ -5,20 +5,23 @@ import Button from '../ui/Button/Button';
 import { useEffect, useState } from "react";
 
 export default function CookieAlert(){
-    const [cookie, setCookie] = useState(false)
+    const [cookie, setCookie] = useState(true)
     useEffect(()=>{
         if (Cookies.get('AsseptCookie') === "True"){
+            setCookie(false)
+        }
+        else{
             setCookie(true)
         }
     },[cookie])
     function AsseptCookie() {
         Cookies.set('AsseptCookie', 'True', { expires: 7 })
-        setCookie(true)
+        setCookie(false)
     }
-    return !cookie ? (
+    return cookie ? (
         <div className={classes.CookieAlert}>
             <p>Мы используем куки, чтобы наш сайт был максимально удобным для вас</p>
-            <Button data={{content:"Понятно"}} onClick={()=>AsseptCookie()}/>
+            <Button className={classes.button} data={{content:"Понятно"}} onClick={()=>AsseptCookie()}/>
         </div>
     ) : (<></>)
 }
