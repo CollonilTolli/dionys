b4f395108ddab2f867acc3b2eeaf1ca4cb1a9920
Nikita Isaev <nikita.isaev@saltpepper.ru>
2024-03-22 13:27:58 +0300

    remove error balance

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,21 @@
-import CookieAlert from "@/components/CookieAlert/CookieAlert"
-import "./globals.scss"
-import ErrorBalance from "@/components/ErrorBalance/ErrorBalance"
+import CookieAlert from "@/components/CookieAlert/CookieAlert";
+import "./globals.scss";
+import ErrorBalance from "@/components/ErrorBalance/ErrorBalance";
 
 export default function RootLayout({
-    children,
+  children,
 }: {
-    children: React.ReactNode
+  children: React.ReactNode;
 }) {
-    return (
-        <html lang="ru">
-            <body>
-                <ErrorBalance />
-                <div className="d-none">
-                    {children}
-                    <CookieAlert />
-                </div>
-            </body>
-        </html>
-    )
+  return (
+    <html lang="ru">
+      <body>
+        {/* <ErrorBalance /> */}
+        {/* <div className="d-none"> */}
+        {children}
+        <CookieAlert />
+        {/* </div> */}
+      </body>
+    </html>
+  );
 }
